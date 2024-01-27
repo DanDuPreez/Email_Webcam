577571712b24f08690e5e87bcf4de02543e93bef
Daniel du Preez <dandupreez@legiondev.co.za>
2024-01-27 11:39:15 +0200

    Send email with attachment

--- emailing.py
+++ emailing.py
@@ -1,3 +1,29 @@
-def send_email():
-    print("Email was sent.")
+import smtplib
+import imghdr
+from email.message import EmailMessage
 
+PASSWORD = "eyot qwsi zxau dgsd"
+SENDER = "dandupreez@legiondev.dev"
+RECEIVER = "dandupreez@legiondev.dev"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import cv2
 import time
 from emailing import send_email
@@ -7,6 +8,7 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -32,12 +34,17 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     cv2.imshow('My Video', frame)
     key = cv2.waitKey(1)
